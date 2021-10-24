ec9f651426466597f0d6ed83adf3a371957bd6b4
JairMarquez1 <a19310186@ceti.mx>
2021-10-24 08:47:02 -0500

    MVC

--- Lima Forsch/src/routes/index.js
+++ Lima Forsch/src/routes/index.js
@@ -1,51 +1,19 @@
 const { Router } = require('express');
 const router = Router();
-const admin = require('firebase-admin');
-const serviceAccount = require('./ServiceAccountKey.json')
+const db = require('../routes/connection');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-})
+const userController = require('../controllers/userController');
 
 
-const db = admin.firestore();
+router.get('/', (req,res) => {res.render('index');});
 
-router.get('/', (req,res) => {
-    //console.log('Index works!');
-    //res.send('received');
-    res.render('index');
-})
+router.get('/login', (req,res) => {res.render('login');});
 
-router.get('/login', (req,res) => {
-    //console.log('Index works!');
-    //res.send('received');
-    res.render('login');
-})
+router.get('/usercrud', userController.listar_usuarios);
 
-router.post('/iniciar-sesion', (req,res) => {
-    console.log(req.body);
-    res.send('received');
-    const obj = JSON.parse(JSON.stringify(req.body));
-    const nuevoUsuario = {
-        usuario: obj.usuario,
-        pass: obj.contrasena,
-        nivel: 1,
-        estado: 1
-    };
-    db.collection('usuarios').add(nuevoUsuario);
-})
+router.post('/iniciar-sesion', userController.iniciar_sesion);
 
 
-router.get('/usercrud', (req,res) => {
-    listarUsuarios(res);
-})
-
-async function listarUsuarios(res){
-    const usersRef = db.collection('usuarios');
-    const snapshot = await usersRef.limit(10).get();
-    var docs = snapshot.docs.map(doc => doc.data());
-    res.render('usercrud', {usuarios: docs});
-}
 
 
 module.exports = router;
